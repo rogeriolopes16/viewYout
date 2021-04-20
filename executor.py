8e1db1ebad305b178a049801566ec8e86f8737d1
Rogério Lopes <rogeriolopes.udi@gmail.com>
2021-04-20 18:47:37 -0300

    Atualizando parametros de execução do Robô

--- executor.py
+++ executor.py
@@ -3,9 +3,9 @@ from random import randint
 
 if __name__ == '__main__':
     # Controles de execução
-    tempo_minimo_video = randint(39, 60)
-    tempo_maximo_video = randint(70, 122)
-    quantidade_execucoes = randint(5, 8)
+    tempo_minimo_video = randint(500, 700)
+    tempo_maximo_video = randint(900, 1100)
+    quantidade_execucoes = randint(8, 15)
 
     openYoutube = YoutubeBot(tempo_minimo_video, tempo_maximo_video)
     i = 0
@@ -16,6 +16,6 @@ if __name__ == '__main__':
         openYoutube.watch_video()
         i += 1
         if i < quantidade_execucoes:
-            time.sleep(randint(555, 1111))  # Tempo que aguardará para proxima execução
+            time.sleep(randint(700, 1000))  # Tempo que aguardará para proxima execução
 
     openYoutube.finaliza()
