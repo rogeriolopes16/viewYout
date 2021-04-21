636fa8ef4c1d4753308b440eb2fcf0d995bd9f64
Rogério Lopes <rogeriolopes.udi@gmail.com>
2021-04-21 00:14:51 -0300

    Inclusão de nos print de informação

--- executor.py
+++ executor.py
@@ -5,11 +5,11 @@ if __name__ == '__main__':
     # Controles de execução
     tempo_minimo_video = randint(500, 700)
     tempo_maximo_video = randint(900, 1100)
-    quantidade_execucoes = randint(8, 15)
+    quantidade_execucoes = randint(7, 12)
 
     openYoutube = YoutubeBot(tempo_minimo_video, tempo_maximo_video)
     i = 0
-
+    print(f"Quantidade de vezes a executar: {quantidade_execucoes}.")
     # Execuções dos videos
     while i < quantidade_execucoes:
         print("Execução: " + str(i + 1))
