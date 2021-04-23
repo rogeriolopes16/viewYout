90e2f7ff9f3f8a57c4fedd6678c881837fb32258
Rogério Lopes <rogeriolopes.udi@gmail.com>
2021-04-23 10:14:18 -0300

    Alteração de biblioteca de manipulação de teclado e mouse

--- modules/viewYoutube.py
+++ modules/viewYoutube.py
@@ -2,16 +2,15 @@ import os
 import sys
 from selenium import webdriver
 import time
-from pynput.mouse import Button, Controller
 from random import randint, shuffle
-import pyperclip
-
+import pyautogui
+from parameters.parameters import *
 
 class YoutubeBot:
     def __init__(self, tempo_minimo_video, tempo_maximo_video):
         self.driver = webdriver.Firefox(executable_path=r"d:/Rogerio/projects/viewYoutube/tools/geckodriver.exe")
-        self.username = "admin"
-        self.password = "4dbf8251"
+        self.username = user_internet
+        self.password = pass_internet
         self.tempo_minimo_video = tempo_minimo_video
         self.tempo_maximo_video = tempo_maximo_video
 
@@ -19,42 +18,29 @@ class YoutubeBot:
         driver = self.driver
         print("Restart Internet.")
         try:
-            driver.get("http://192.168.15.1/")  # abre configurações do modem
+            driver.get(ip_internet)  # abre configurações do modem
             time.sleep(5)
-            mouse = Controller()
-            mouse.position = (247, 354)  # posiciona ponteiro no menu comfigurações
-            mouse.click(Button.left, 1)  # clica no posicionamento acima
+
+            pyautogui.click(247, 354)  # posiciona ponteiro no menu comfigurações
             time.sleep(1)
-            mouse.position = (203, 476)  # posiciona ponteiro no submenu reiniciar
-            mouse.click(Button.left, 1)  # clica no posicionamento acima
-            time.sleep(5)
-
-            pyperclip.copy(self.username)
-            mouse.position = (623, 364)  # posiciona ponteiro no campo usuario
-            mouse.click(Button.right, 1)  # clica botão direito
-            time.sleep(2)
-            mouse.position = (699, 477)  # posiciona ponteiro na opção colar
-            mouse.click(Button.left, 1)  # clica no posicionamento acima
-            time.sleep(2)
-
-            pyperclip.copy(self.password)
-            mouse.position = (643, 406)  # posiciona ponteiro na seleção da senha
-            mouse.click(Button.right, 1)  # clica botão direito
-            time.sleep(2)
-            mouse.position = (693, 524)  # posiciona ponteiro na opção colar
-            mouse.click(Button.left, 1)  # clica no posicionamento acima
-            time.sleep(2)
-
-            mouse.position = (1080, 441)  # posiciona ponteiro no botão entrar
-            mouse.click(Button.left, 1)  # clica no posicionamento acima
+            pyautogui.click(203, 476)  # posiciona ponteiro no submenu reiniciar
             time.sleep(3)
 
-            mouse.position = (443, 331)  # posiciona ponteiro no botão reiniciar
-            mouse.click(Button.left, 1)  # clica no posicionamento acima
+            pyautogui.click(623, 364)  # posiciona ponteiro no campo usuario
+            pyautogui.write(self.username)
             time.sleep(1)
 
-            mouse.position = (607, 499)  # posiciona ponteiro no botão confirma reiniciar
-            mouse.click(Button.left, 1)  # clica no posicionamento acima
+            pyautogui.click(643, 406)  # posiciona ponteiro na seleção da senha
+            pyautogui.write(self.password)
+            time.sleep(1)
+
+            pyautogui.press('enter')  # enter no botão entrar
+            time.sleep(3)
+
+            pyautogui.click(443, 331)  # posiciona ponteiro no botão reiniciar
+            time.sleep(1)
+
+            pyautogui.click(607, 499)  # posiciona ponteiro no botão confirma reiniciar
             time.sleep(150)
         except:
             driver = self.driver
@@ -79,9 +65,7 @@ class YoutubeBot:
                 tempo = randint(self.tempo_minimo_video, self.tempo_maximo_video)
                 print("Assistindo o video: " + video + " por " + str(tempo) + " segundos.")
                 time.sleep(3)
-                mouse = Controller()
-                mouse.position = (433, 398)  # posiciona ponteiro do mouse no botão de play
-                mouse.click(Button.left, 1)  # clica no posicionamento acima para iniciar o video
+                pyautogui.click(433, 398)  # posiciona ponteiro do mouse no botão de play
                 time.sleep(tempo)  # Tempo que irá assistir o video
         except:
             driver = self.driver
@@ -95,6 +79,5 @@ class YoutubeBot:
 
     @staticmethod
     def captura_ponteiro():
-        mouse = Controller()
-        time.sleep(2)
-        print('The current pointer position is {0}'.format(mouse.position))
+        time.sleep(3)
+        print('The current pointer position is {0}'.format(pyautogui.position()))
